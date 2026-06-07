658228dad6f48af2cb68226d23854e80648011f8
Niklas Eldberger <niklas.eldberger@gmail.com>
2026-06-07 21:33:27 +0200

    Enable OAS3Deserializer to deserialize operation where requestBody schema has $ref to #/components/schemas

---
+++ openapi/src/test/java/com/zuunr/api/openapi/OAS3DeserializerTest.java
@@ -0,0 +1,56 @@
+package com.zuunr.api.openapi;
+
+import com.zuunr.json.JsonObject;
+import com.zuunr.json.JsonValue;
+import com.zuunr.jsontester.GivenWhenThenTesterBase;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+
+public class OAS3DeserailizerTest extends GivenWhenThenTesterBase {
+
+    private JsonObject givenOpenApiDefAndPathToOperation;
+
+    /*
+     * This method implementation may be copied as-is to any other subclass of GivenWhenThenBaseTester
+     */
+    static Stream<Path> testFiles() throws Exception {
+        return testFiles((Class<? extends GivenWhenThenTesterBase>) new Object() {
+        }.getClass().getEnclosingClass()); // NOSONAR
+    }
+
+    /*
+     * This method implementation and annotations may be copied as-is to any other subclass of GivenWhenThenBaseTester
+     */
+    @DisplayName("Run test for each JSON file")
+    @ParameterizedTest(name = "{index} => JSON file: {0}")
+    @MethodSource("testFiles")
+    void test(Path testsFolderPath) throws Exception {
+        executeTest(testsFolderPath);
+    }
+
+    @Override
+    public void doGiven(JsonValue given) {
+        givenOpenApiDefAndPathToOperation = given.getJsonObject();
+    }
+
+    @Override
+    public JsonValue doWhen(JsonValue whenExchangeWithRequest) {
+
+        JsonObject result = OAS3Deserializer.deserializeRequest(
+                whenExchangeWithRequest.getJsonObject(),
+                givenOpenApiDefAndPathToOperation.get("openApiDef").getJsonObject(),
+                givenOpenApiDefAndPathToOperation.get("pathToOperation").getJsonArray());
+
+        return result.jsonValue();
+    }
+}
