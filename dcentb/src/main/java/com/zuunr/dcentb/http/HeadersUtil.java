4ade06214bd08a62dc897e3f343fc99a9b73d0ab
Niklas Eldberger <niklas.eldberger@gmail.com>
2026-08-14 06:57:03 +0200

    Ugrade to Spring boot 4.1 and removal of some subprojects

--- dcentb/src/main/java/com/zuunr/dcentb/http/HeadersUtil.java
+++ dcentb/src/main/java/com/zuunr/dcentb/http/HeadersUtil.java
@@ -16,7 +16,7 @@ public class HeadersUtil {
     public static JsonObject translateHttpHeaders(HttpHeaders httpHeaders) {
 
         JsonObjectBuilder builder = JsonObject.EMPTY.builder();
-        for (Iterator<Map.Entry<String, List<String>>> iterator = httpHeaders.entrySet().iterator(); iterator.hasNext(); ) {
+        for (Iterator<Map.Entry<String, List<String>>> iterator = httpHeaders.headerSet().iterator(); iterator.hasNext(); ) {
             Map.Entry<String, List<String>> entry = iterator.next();
             JsonArray values = JsonArray.of(entry.getValue().toArray()); // TODO: Iterate the list directly to avoid extra loop
             String headerName = entry.getKey();
